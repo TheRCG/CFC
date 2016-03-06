2c510506e1c9a0067070963eeaf15f8ddc48264a
TheRCG <TheRCG@users.noreply.github.com>
2016-03-06 20:31:39 +0300

    Update some code

--- src/alert.h
+++ src/alert.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2009-2016 The Bitcoin developers
+// Copyright (c) 2014-2016 TheRCG developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,7 +9,6 @@
 
 #include <set>
 #include <string>
-
 #include "uint256.h"
 #include "util.h"
 
